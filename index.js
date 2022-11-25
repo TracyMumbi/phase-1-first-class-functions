d71063847730778cdae6ef326839fa531b3a8e7a
Tracy Mumbi <tracy.mumbi@student.moringaschool.com>
2022-11-25 09:09:42 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,28 @@
+const receivesAFunction = (call) => {
+    call();
+}
+
+const returnsANamedFunction = (fn = returnsANamedFunction) => {
+    return returnsANamedFunction;
+}
+
+const returnsAnAnonymousFunction = (fn = returnsAnAnonymousFunction) => {
+    return (function () {
+    })
+}
+
+
+// function receivesAFunction(call) {
+//     call();
+// }
+
+// function returnsANamedFunction(fn = returnsANamedFunction) {
+//     return returnsANamedFunction;
+// }
+
+// function returnsAnAnonymousFunction() {
+//     return (function() {
+
+//     })
+// }
+    
